417283d03424ab9da5af45e4d4aeddb0b97b50ee
rifqiAly <rifqi.aly@gmail.com>
2023-01-25 09:34:45 +0700

    initial data, instated, DONE

---
+++ jadwal-sholat-fe/src/helper/master.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const ListJadwalSholat = async (currentDate) => {
+    let url = `https://api.banghasan.com/sholat/format/json/jadwal/kota/703/tanggal/${currentDate}`;
+    let res = await axios.get(url);
+    return res;
+};
+
+const ConvertTanggalHijriyah = async (currentDate) => {
+  const options = {
+    method: 'GET',
+    url: 'https://masehi-ke-hijriyah.p.rapidapi.com/',
+    params: {tanggal: `${currentDate}`},
+    headers: {
+      'X-RapidAPI-Key': '293a1d9141mshb9de18ba68db94cp1b21fajsnc40fc37db137',
+      'X-RapidAPI-Host': 'masehi-ke-hijriyah.p.rapidapi.com'
+    }
+  };
+  
+  let res = await axios.request(options)
+  return res
+};
+
+
+export default {
+  ListJadwalSholat,
+  ConvertTanggalHijriyah
+}
